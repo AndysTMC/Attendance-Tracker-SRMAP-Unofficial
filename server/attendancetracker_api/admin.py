6d2ff7cc49812a98e79741587dafce291a26b5e8
AndysTMC <thatikondamukesh08@gmail.com>
2023-10-02 15:51:32 +0530

    Removed server

--- server/attendancetracker_api/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from .models import AttendanceTracker, WebActions, Utils
-
-# Register your models here.
--- server/attendancetracker_api/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class AttendancetrackerApiConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'attendancetracker_api'
